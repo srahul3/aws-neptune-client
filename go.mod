29f6a59d942438aa069af5bc75d67e3971482ce3
srahul3 <rahulsharma@hashicorp.com>
2024-08-28 21:04:15 +0530

    neptune client

--- go.mod
+++ go.mod
@@ -5,6 +5,22 @@ go 1.22.2
 require github.com/apache/tinkerpop/gremlin-go/v3 v3.7.2
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.30.4 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.30 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.12 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.16 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.16 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.18 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.22.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.30.5 // indirect
+	github.com/aws/smithy-go v1.20.4 // indirect
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.27.31
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -1,22 +1,100 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+
+// 	gremlingo "github.com/apache/tinkerpop/gremlin-go/v3/driver"
+// )
+
+// func main() {
+
+// 	// get metune endpoint from environment variable
+// 	endpoint := os.Getenv("NEPTUNE_ENDPOINT")
+
+// 	// Creating the connection to the server.
+// 	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection("wss://"+endpoint+":8182/gremlin",
+// 		func(settings *gremlingo.DriverRemoteConnectionSettings) {
+// 			settings.TraversalSource = "g"
+// 		})
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	// Cleanup
+// 	defer driverRemoteConnection.Close()
+
+// 	// Creating graph traversal
+// 	g := gremlingo.Traversal_().WithRemote(driverRemoteConnection)
+
+// 	// Perform traversal
+// 	results, err := g.V().Limit(2).ToList()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	// Print results
+// 	for _, r := range results {
+// 		fmt.Println(r.GetString())
+// 	}
+// }
+
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	gremlingo "github.com/apache/tinkerpop/gremlin-go/v3/driver"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
 )
 
 func main() {
+	ctx := context.Background()
+	db_endpoint := os.Getenv("NEPTUNE_ENDPOINT")
+	connString := "wss://" + db_endpoint + ":8181/gremlin"
 
-	// get metune endpoint from environment variable
-	endpoint := os.Getenv("NEPTUNE_ENDPOINT")
+	// Signing Request
+	const emptyStringSHA256 = `e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855`
+	req, err := http.NewRequest(http.MethodGet, connString, strings.NewReader(""))
+	if err != nil {
+		return
+	}
 
-	// Creating the connection to the server.
-	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection("wss://"+endpoint+":8182/gremlin",
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		fmt.Println(fmt.Errorf("unable to load AWS SDK config: %w", err))
+	}
+	cr, err := cfg.Credentials.Retrieve(ctx)
+	if err != nil {
+		fmt.Println(fmt.Errorf("unable to retrieve AWS credentials: %w", err))
+	}
+	signer := v4.NewSigner()
+
+	gen := func() gremlingo.AuthInfoProvider {
+		err := signer.SignHTTP(ctx, cr, req, emptyStringSHA256, "db-neptune-ig", "us-west-2", time.Now())
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		return gremlingo.HeaderAuthInfo(req.Header)
+	}
+
+	auth := gremlingo.NewDynamicAuth(gen)
+
+	driverRemoteConnection, err := gremlingo.NewDriverRemoteConnection(connString,
 		func(settings *gremlingo.DriverRemoteConnectionSettings) {
 			settings.TraversalSource = "g"
+			settings.AuthInfo = auth
+			//settings.TlsConfig = &tls.Config{InsecureSkipVerify: true} Use this only if you're on a Mac running Go 1.18+ doing local dev. See https://github.com/golang/go/issues/51991
+
 		})
+
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -27,14 +105,23 @@ func main() {
 	// Creating graph traversal
 	g := gremlingo.Traversal_().WithRemote(driverRemoteConnection)
 
-	// Perform traversal
-	results, err := g.V().Limit(2).ToList()
+	// Add a vertex with properties to the graph with the terminal step Iterate()
+	promise := g.AddV("gremlin").Property("language", "go").Iterate()
+
+	// The returned promised is a go channel to wait for all submitted steps to finish execution and return error.
+	err = <-promise
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	// Print results
-	for _, r := range results {
+	// Get the value of the property
+	result, err := g.V().HasLabel("gremlin").Values("language").ToList()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Print the result
+	for _, r := range result {
 		fmt.Println(r.GetString())
 	}
 }
